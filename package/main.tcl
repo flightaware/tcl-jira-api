1a5a4e28f88e8666a7d36f8cdc7658f93aab6997
Philip Clifton <p.clifton@squawk1200.net>
2017-03-14 22:16:02 +0000

    Explicitly specify request type
    ::jira::raw now requires callers to specify a request type. All callers were updated to provide this info. Also, doTransition was tweaked to work correctly.

--- package/main.tcl
+++ package/main.tcl
@@ -52,17 +52,20 @@ namespace eval ::jira {
 		return 1
 	}
 
-	proc raw {url _result args} {
+	proc raw {url {method GET} _result args} {
 		::jira::parse_args args argarray
 
 		upvar 1 $_result result
 		unset -nocomplain result
 
-		if {[info exists argarray(post)]} {
-			set token [::http::geturl $url -headers [::jira::authheaders] -query $argarray(post)]
-		} else {
-			set token [::http::geturl $url -headers [::jira::authheaders]]
+		set httpArgs [list $url -headers [::jira::authheaders] -method $method]
+		# Add body if provided
+		if {[info exists argarray(body)]} {
+			lappend httpArgs -query $argarray(body)
 		}
+		
+		# Do the request
+		set token [::http::geturl {*}$httpArgs]
 		::http::wait $token
 
 		foreach k {data error status code ncode size meta} {
@@ -91,7 +94,7 @@ namespace eval ::jira {
 
 		set url "[::jira::baseurl]/rest/api/2/issue/createmeta"
 
-		if {[::jira::raw $url json]} {
+		if {[::jira::raw $url GET json]} {
 			array set rawresult [::yajl::json2dict $json(data)]
 			foreach p $rawresult(projects) {
 				unset -nocomplain project
@@ -127,7 +130,7 @@ namespace eval ::jira {
 
 		puts "URL $url"
 
-		if {[::jira::raw $url json]} {
+		if {[::jira::raw $url GET json]} {
 			array set result [::yajl::json2dict $json(data)]
 			# parray result
 			return 1
@@ -142,7 +145,7 @@ namespace eval ::jira {
 
 		set url "[::jira::baseurl]/rest/api/2/applicationrole"
 
-		if {[::jira::raw $url json]} {
+		if {[::jira::raw $url GET json]} {
 			array set result [::yajl::json2dict $json(data)]
 			# parray result
 			return 1
@@ -157,7 +160,7 @@ namespace eval ::jira {
 
 		set url "[::jira::baseurl]/rest/api/2/issue/$issueID/transitions"
 
-		if {[::jira::raw $url json]} {
+		if {[::jira::raw $url GET json]} {
 			array set result [::yajl::json2dict $json(data)]
 			# parray result
 			return 1
@@ -166,7 +169,7 @@ namespace eval ::jira {
 		}
 	}
 
-	proc doTransition {issueID transition _result args} {
+	proc doTransition {issueID transition _result} {
 		upvar 1 $_result result
 		unset -nocomplain result
 
@@ -185,12 +188,16 @@ namespace eval ::jira {
 			set url "[::jira::baseurl]/rest/api/2/issue/$issueID/transitions"
 
 			set postdata [::yajl create #auto]
-			$postdata map_open string body string $argarray(body)
-			# $postdata string visibility map_open string type string role string value string developers map_close
+			$postdata map_open
+				$postdata map_key transition map_open
+					$postdata map_key id string $transitionID
+				$postdata map_close
+			$postdata map_close
+			
+			set jsonpost [$postdata get]
+			$postdata delete
 
-			$postdata string author map_open
-
-			if {[::jira::raw $url json]} {
+			if {[::jira::raw $url POST json -body $jsonpost]} {
 				array set result [::yajl::json2dict $json(data)]
 				# parray result
 				return 1
@@ -203,7 +210,7 @@ namespace eval ::jira {
 	proc getItemID {type name {field "name"}} {
 		set url "[::jira::baseurl]/rest/api/2/$type"
 
-		if {[::jira::raw $url json]} {
+		if {[::jira::raw $url GET json]} {
 			foreach el [::yajl::json2dict $json(data)] {
 				unset -nocomplain item
 				array set item $el
@@ -225,7 +232,7 @@ namespace eval ::jira {
 
 			set keylist [list]
 
-			if {[::jira::raw $url json]} {
+			if {[::jira::raw $url GET json]} {
 				foreach el [::yajl::json2dict $json(data)] {
 					unset -nocomplain item
 					array set item $el
@@ -303,7 +310,7 @@ namespace eval ::jira {
 			puts "POST $jsonpost"
 		}
 
-		if {[::jira::raw $url json -post $jsonpost]} {
+		if {[::jira::raw $url POST json -body $jsonpost]} {
 			array set result [::yajl::json2dict $json(data)]
 			return 1
 		} else {
@@ -344,7 +351,7 @@ namespace eval ::jira {
 			puts "POST $jsonpost"
 		}
 
-		if {[::jira::raw $url json -post $jsonpost]} {
+		if {[::jira::raw $url POST json -body $jsonpost]} {
 			array set result [::yajl::json2dict $json(data)]
 			# parray result
 			return 1
